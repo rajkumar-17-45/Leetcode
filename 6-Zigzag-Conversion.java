13a0fbcac068a4e69a17a45360116f9ec0399a9d
rajkumar-17-45 <rajkumarperiyasamy005@gmail.com>
2024-09-30 22:31:12 +0530

    [6] [Time Beats: 33.68%] [Memory Beats: 37.76%] - LeetPush

---
+++ 6-Zigzag-Conversion.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        String arr[]=new String[numRows];
+        for(int i=0;i<numRows;i++)
+        {
+            arr[i]=\\;
+        }
+        int x=0;
+        int f=1;
+        while(x<s.length())
+        {
+          if(f==1)
+          {
+            for(int i=0;i<numRows;i++)
+            {
+              
+                if(x<s.length())
+               arr[i]+=String.valueOf(s.charAt(x));
+                x++;
+            }
+            f=0;
+          }
+          else if(f==0)
+          {
+            for(int i=numRows-2;i>0;i--)
+            {
+              
+                if(x<s.length())
+               arr[i]+=String.valueOf(s.charAt(x));
+                x++;
+            }
+            f=1;
+          }
+        //   x++;
+        }
+        String babluragu=new String();
+        for(String c:arr)
+        babluragu+=c;
+        return babluragu;
+    }
+}
