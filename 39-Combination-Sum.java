74182b77e940959eada586be0fcd30b0113a9f5d
rajkumar-17-45 <rajkumarperiyasamy005@gmail.com>
2024-10-01 21:15:15 +0530

    [39] [Time Beats: 99.86%] [Memory Beats: 68.83%] - LeetPush

--- 39-Combination-Sum.java
+++ 39-Combination-Sum.java
@@ -1,42 +1,29 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-
 class Solution {
-    static List<List<Integer>> ans = new ArrayList<>();
-
-    List<List<Integer>> comsum(List<Integer> a, int target) {
-        ArrayList<Integer> temp = new ArrayList<>();
-        HashSet<Integer> set = new HashSet<>(a);
-        a.clear();
-        a.addAll(set);
-        Collections.sort(a);
-        findnumbers(a, temp, target, 0);
-        return ans;
+    List<List<Integer>> li=new ArrayList<List<Integer>>();
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        List<Integer> ds=new ArrayList<Integer>();
+        backtrack(nums,ds,target,0);
+        return li;
     }
-
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        ans.clear(); 
-        List<Integer> a = new ArrayList<>();
-        for (int i : candidates) {
-            a.add(i);
-        }
-        comsum(a, target);
-        return ans;
-    }
-
-    void findnumbers(List<Integer> a, List<Integer> temp, int target, int index) {
-        if (target == 0) {
-            ans.add(new ArrayList<>(temp)); 
-            return;
-        }
-        for (int i = index; i < a.size(); i++) {
-            if (target - a.get(i) >= 0) {
-                temp.add(a.get(i)); 
-                findnumbers(a, temp, target - a.get(i), i);
-                temp.remove(temp.size() - 1); 
+    void backtrack(int nums[],List<Integer> ds,int target,int i)
+    {
+        if(i==nums.length )
+        {
+            if(target==0)
+            {
+              li.add(new ArrayList<Integer>(ds));
             }
+            return ;
         }
+        // for( i=0;i<nums.length;i++)
+        // {
+        if(target>=nums[i])
+        { 
+         ds.add(nums[i]);
+         backtrack(nums,ds,target-nums[i],i);
+       ds.remove(ds.size()-1);
+    }
+     backtrack(nums,ds,target,i+1);
+        // }
     }
 }
