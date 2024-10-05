a5f9285bf3868ca16d1eb8f5314f781612423fc1
rajkumar-17-45 <rajkumarperiyasamy005@gmail.com>
2024-10-05 10:36:07 +0530

    [567] [Time Beats: 20.44%] [Memory Beats: 22.66%] - LeetPush

--- DCP-10-24/567-Permutation-in-String.java
+++ DCP-10-24/567-Permutation-in-String.java
@@ -8,35 +8,28 @@ class Solution {
         {
             freq[s1.charAt(i)-'a']++;
         }
-        int sum1=0;
-        for(int i=0;i<26;i++)
-        {
-            sum1+=freq[i];
-        }
         for(int i=0;i<n-m+1;i++)
         {
             String lp=new String();
             // int y=0;
+            int[] z=freq.clone();
             int freq2[]=new int[26];
             for(int k=i;k<i+m && k<n;k++)
             {
-                freq2[s2.charAt(k)-'a']++;
-                // lp+=s2.charAt(k);
+                z[s2.charAt(k)-'a']--;             
             }
-          if(check(freq,freq2))
+          if(check(z))
           return true;
         }
         return false;
     }
-   boolean check(int[] freq,int[] freq2)
+   boolean check(int[] freq)
    {
-    
-    
     boolean v;
     for(int i=0;i<26;i++)
     {
-     if(freq[i]!=freq2[i])
-     return false;
+     if(freq[i]>0)
+    return false;
     }
     return true;
    }
