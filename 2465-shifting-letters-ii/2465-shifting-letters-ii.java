bb6cc5a121804dfcafc0490f943b70d9a906631c
rajkumar-17-45 <rajkumarperiyasamy005@gmail.com>
2025-01-05 13:48:24 +0530

    Time: 262 ms (7.55%), Space: 67.4 MB (100%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int n=shifts.length;
+        int arr[]=new int[s.length()];
+        int prefix[]=new int[s.length()+1];
+        for(int i=0;i<n;i++)
+        {
+            int a=shifts[i][0];
+            int b=shifts[i][1];
+            int c=shifts[i][2];
+            if(c==0)
+            {
+                prefix[a]+=-1;
+                prefix[b+1]+=1;
+            }
+             else 
+            {
+                prefix[a]+=1;
+                prefix[b+1]+=-1;
+            }
+        }
+        int sum=0;
+        String res=new String();
+        for(int i=0;i<prefix.length;i++)
+        {
+            sum+=prefix[i];
+            prefix[i]=sum;
+        }
+        // String res=new String();
+        for(int i=0;i<s.length();i++)
+        {
+           char c=(char)(((s.charAt(i)+(prefix[i])%26-97+26)%26)+97);
+           res+=c;
+        }
+        // System.out.println(Arrays.toString(prefix));
+        return res;
+    }
+}
