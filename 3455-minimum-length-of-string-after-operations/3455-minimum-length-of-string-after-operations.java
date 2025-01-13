337c8990f79e537bc9f0085fad34a5b416eabab7
rajkumar-17-45 <rajkumarperiyasamy005@gmail.com>
2025-01-13 23:09:02 +0530

    Time: 164 ms (5.3%), Space: 46 MB (89.4%) - LeetHub

---
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumLength(String s) {
+       HashMap<Character,Integer> mp=new HashMap<Character,Integer>();
+       for(char c:s.toCharArray())
+       {
+        mp.put(c,mp.getOrDefault(c,0)+1);
+        if(mp.get(c)==3)
+        mp.put(c,1);
+       }
+       int res=0;
+       System.out.println(mp);
+       for(char key:mp.keySet())
+       res+=mp.get(key);
+       return res;
+    }
+}
