4bde4b9dbcd021876fe8ad47ccb3b3bc862cda2b
rajkumar-17-45 <rajkumarperiyasamy005@gmail.com>
2025-01-23 23:58:12 +0530

    Time: 0 ms (100%), Space: 41.1 MB (67.52%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -21,11 +21,7 @@ class Solution {
     {
         if(p==null && q==null)
         return true;
-        if(p==null && q!=null)
-        return false;
-        if(p!=null && q==null)
-        return false;
-        if(p.val!=q.val)
+        if((p!=null && q==null)||(p==null && q!=null) ||(p.val!=q.val))
         return false;
         return sametree(p.left,q.left) && sametree(p.right,q.right);
     }
