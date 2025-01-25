9a9bbd5e97e791ec2873c347fb838c5e18094096
rajkumar-17-45 <rajkumarperiyasamy005@gmail.com>
2025-01-25 21:17:53 +0530

    Time: 0 ms (100%), Space: 43.5 MB (26.34%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,20 +14,18 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int sum) {
-        if(root==null)
-        return false;
-       return pathsum(root,sum);
-    }
-    boolean pathsum(TreeNode root,int sum)
-    {
-        if(root==null)
+    public boolean hasPathSum(TreeNode root, int t) {
+       return dfs(root,t);        
+}
+   boolean dfs(TreeNode root,int t)
+   {
+       if(root==null)
        return false;
-       if(root.left==null && root.right==null)
-       {
-             return (sum-root.val)==0;
-       }
-       
-       return pathsum(root.left,sum-root.val) || pathsum(root.right,sum-root.val);
-    }
+      boolean b=dfs(root.left,t-root.val);
+      boolean c=dfs(root.right,t-root.val);
+    //   System.out.println(t+" "+root.val);
+       if(t-root.val==0 && root.left==null && root.right==null)
+       return true;
+       return  b|c;
+   }
 }
